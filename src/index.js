bd6853eaa7ed8dab73097659de8fecf9aee55034
Aliaksandr Kazerski <sashfob@mail.ru>
2019-02-24 18:38:11 +0300

    add function Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = [];
+  arr.push(str[0]);
+  for (var i = 1; i < str.length; i++) {
+    arr.push(str[i]);
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (JSON.stringify([arr[arr.length - 2], arr[arr.length - 1]]) === JSON.stringify(bracketsConfig[j])) {
+        arr.pop();
+        arr.pop();
+      }
+    }
+  }
+  if (arr.length === 0) {
+    return true;
+  }
+  return false;    
 }
